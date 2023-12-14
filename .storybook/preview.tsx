460b5412c073c2161181ac81a782d778e8b7566b
ienrum <azang0506@gmail.com>
2023-12-14 11:00:08 +0900

    👔 : storybook 에 recoil 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,16 @@
 import type { Preview } from '@storybook/react';
+import React from 'react';
+import { RecoilRoot } from 'recoil';
 import '../src/index.css';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
