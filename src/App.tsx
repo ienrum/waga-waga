bef21dddd8af28608a978c90b7783a28ca47cba0
ienrum <azang0506@gmail.com>
2023-12-14 11:11:55 +0900

    ♻️ : 보일러 플레이트 제거맟 fallback(로딩) 을 Bots 에서 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,29 @@
-import WagaBots from './components/WagaBots';
-import React, { useEffect, useState, useRef } from 'react';
+import { useRef } from 'react';
 import TokenForm from './components/TokenForm';
 import { useRecoilState } from 'recoil';
-import { fetchErrorState, isTalkingState } from './store/atom';
-import RestartButton from './components/RestartButton';
-import LoadingBots from './components/LoadingBots.tsx';
+import { fetchErrorState } from './store/atom';
+import Bots from './components/Bots.tsx';
 
 function App() {
   const [fetchError, _] = useRecoilState(fetchErrorState);
-  const [isTalking, setIsTalking] = useRecoilState(isTalkingState);
   const count = useRef(0);
 
   count.current += 1;
-
-  const botName = ['waga', 'woba', 'mogo'];
   return (
-    <React.Suspense
-      fallback={
-        <div className='flex h-screen items-center bg-slate-600 pb-24'>
-          <LoadingBots />
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-8 bg-slate-600'>
+      {fetchError && (
+        <div className='flex h-full w-full flex-col items-center justify-center gap-8 bg-slate-600'>
+          <div className='text-4xl'>Invalid token</div>
+          <TokenForm />
         </div>
-      }>
-      <div className='relative flex h-screen items-center bg-slate-600 pb-24'>
-        {fetchError && (
-          <div className='flex h-full w-full flex-col items-center justify-center gap-8'>
-            <div className='text-4xl'>Invalid token</div>
-            <TokenForm />
-          </div>
-        )}
-        {!fetchError && <WagaBots key={count.current} />}
-        {!isTalking && <RestartButton />}
-      </div>
-    </React.Suspense>
+      )}
+      {!fetchError && (
+        <Bots
+          key={count.current}
+          initialMessage={['hi', 'whats up?', 'nice to seeyah']}
+        />
+      )}
+    </div>
   );
 }
 
