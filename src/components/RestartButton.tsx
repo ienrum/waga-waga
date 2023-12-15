e7132c7bf360a6d8204b7871e5362f36b381d9b0
ienrum <azang0506@gmail.com>
2023-12-15 17:45:17 +0900

    🐛 : restartCount 변수를 클릭시에 count 하도록해서 Bots 에 key 를 전달

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -1,14 +1,17 @@
 import { useSetRecoilState } from 'recoil';
 import { countState } from '@/components/Bots';
+import { restartCountState } from '@/App';
 
 export default function RestartButton() {
   const setCount = useSetRecoilState(countState);
+  const setRestartCount = useSetRecoilState(restartCountState);
 
   return (
     <button
       className=' absolute left-3/4 top-1/4 rounded-md bg-slate-700 p-2 text-slate-200 hover:bg-slate-400 active:bg-slate-950'
       onClick={() => {
         setCount(0);
+        setRestartCount((prev) => prev + 1);
       }}>
       Restart
     </button>
